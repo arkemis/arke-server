a72eceefc1393a267d6e6c80733d424b15fa555a
Ilyich Vismara <ilyich@arkemis.it>
2026-04-14 17:30:11 +0200

    chore: update arke deps

--- mix.exs
+++ mix.exs
@@ -69,9 +69,9 @@ defmodule ArkeServer.MixProject do
       {:ymlr, "~> 2.0", only: :dev},
       {:excoveralls, "~> 0.10", only: :test},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:arke, "~> 0.4.0"},
-      {:arke_postgres, "~> 0.4.0"},
-      {:arke_auth, "~> 0.4.0"},
+      {:arke, "~> 0.6.0"},
+      {:arke_postgres, "~> 0.5.0"},
+      {:arke_auth, "~> 0.4.4"},
       {:hackney, "~> 1.18"},
       {:swoosh, "~> 1.11"}
     ])
