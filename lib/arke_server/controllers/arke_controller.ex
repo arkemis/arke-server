2081714ee8912bba507a4464814693ee686c1dec
Ilyich Vismara <81801314+ilyichv@users.noreply.github.com>
2026-08-03 19:25:22 +0200

    fix: return 404 for unknown arke ids on group (#119)

--- lib/arke_server/controllers/arke_controller.ex
+++ lib/arke_server/controllers/arke_controller.ex
@@ -275,20 +275,22 @@ defmodule ArkeServer.ArkeController do
   """
   def get_groups(conn, %{"arke_id" => id}) do
     project = conn.assigns[:arke_project]
-    offset = Map.get(conn.query_params, "offset", nil)
-    limit = Map.get(conn.query_params, "limit", nil)
-    order = Map.get(conn.query_params, "order", [])
-    arke = ArkeManager.get(id, project)
 
-    {count, units} =
-      QueryManager.query(project: project, arke: :group)
-      |> QueryManager.link(arke, direction: :parent, type: "group")
-      |> QueryFilters.apply_query_filters(Map.get(conn.assigns, :filter))
-      |> QueryProcessor.process_query(conn.query_params)
+    case ArkeManager.get(id, project) do
+      nil ->
+        ResponseManager.send_resp(conn, 404, nil)
 
-    ResponseManager.send_resp(conn, 200, %{
-      count: count,
-      items: StructManager.encode(units, type: :json)
-    })
+      arke ->
+        {count, units} =
+          QueryManager.query(project: project, arke: :group)
+          |> QueryManager.link(arke, direction: :parent, type: "group")
+          |> QueryFilters.apply_query_filters(Map.get(conn.assigns, :filter))
+          |> QueryProcessor.process_query(conn.query_params)
+
+        ResponseManager.send_resp(conn, 200, %{
+          count: count,
+          items: StructManager.encode(units, type: :json)
+        })
+    end
   end
 end
